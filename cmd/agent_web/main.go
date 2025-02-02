017bb7d6102f682f31303470dcd178edfd0046e4
Ming Qian <mqian.coding@gmail.com>
2025-02-02 16:52:33 -0500

    interaction with llm

--- cmd/agent_web/main.go
+++ cmd/agent_web/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"fmt"
+	"github.com/mqian-coding/agent_web/internal/project_manager"
+)
+
+func main() {
+	resp, err := project_manager.DoRequestToAgent("Generate a bash file called hello_world.sh which just echos hello world!", project_manager.DeepseekCoderModelName, "11434")
+	fmt.Println(resp)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
--- internal/project_manager/project_manager.go
+++ internal/project_manager/project_manager.go
@@ -1,12 +1,40 @@
 package project_manager
 
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var (
+	Host = "localhost"
+)
+
+const (
+	DeepseekCoderModelName = "deepseek-coder:33b"
+)
+
+type OllamaResponse struct {
+	Model     string `json:"model"`
+	CreatedAt string `json:"created_at"`
+	Response  string `json:"response"`
+	Done      bool   `json:"done"`
+}
+
 func UserPromptLoop(prompt string) {
 	// TODO: Should continuously listen for user input until the loop is explicitly broken by the user,
 	// 	at which point start the next step of code generation loop
-	//reader := bufio.NewReader(os.Stdin)
-	//for {
-	//	input, _ := reader.ReadString('\n')
-	//}
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		input, _ := reader.ReadString('\n')
+		DoRequestToRefiner(input)
+	}
 }
 
 func DoRequestToRefiner(prompt string) {
@@ -15,9 +43,44 @@ func DoRequestToRefiner(prompt string) {
 	//  If not, return response to User, otherwise Semantic Parser will return requests to be made. Make
 	//  the requests and send results back to the Refiner Agent. Loop continues until no more semantic parsing
 	//  is needed and control defers back to the UserPromptLoop.
+
 }
 
 func DoRequestSemanticParser(prompt string) []byte {
-	// TODO:
+	// TODO: Forward the response from the refiner,
 	return nil
+}
+
+func DoRequestToAgent(prompt, model, port string) (string, error) {
+	var err error
+	body := bytes.NewReader([]byte(fmt.Sprintf("{\n\t\"model\": \"%s\",\n\t\"prompt\":\"%s\"\n}", model, prompt)))
+	url := fmt.Sprintf("http://%s:%s/api/generate", Host, port)
+	req, err := http.NewRequest(http.MethodPost, url, body)
+	if err != nil {
+		return "", err
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	nlResp, err := AgentResponseToNaturalLanguage(resp)
+	return nlResp, err
+}
+
+func AgentResponseToNaturalLanguage(resp *http.Response) (string, error) {
+	if resp == nil {
+		return "", errors.New("resp *http.Response is nil")
+	}
+	raw, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	responses := strings.Split(string(raw), "\n")
+	builder := &strings.Builder{}
+	ollamaResp := &OllamaResponse{}
+	for _, response := range responses {
+		json.Unmarshal([]byte(response), ollamaResp)
+		builder.Write([]byte(ollamaResp.Response))
+	}
+	return builder.String(), nil
 }
